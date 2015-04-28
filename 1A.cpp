a746e0349a5e4763433259c8be8030ee47f1b852
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-04-28 12:03:46 +0800

    1A - Theatre Square
    link: http://codeforces.com/problemset/problem/1/A

---
+++ 1A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double a, n, m;
+
+    cin >> n >> m >> a;
+    cout << (long long)(ceil(n/a) * ceil(m/a));
+
+    return 0;
+}

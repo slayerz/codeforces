707a085bae4b3cdfe17533fe74266af31694cb70
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-04-28 18:14:22 +0800

    96A - Football
    link: http://codeforces.com/problemset/problem/96/A

---
+++ 96A.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    string team;
+    int i;
+    int sum0=0, sum1=0;
+    bool danger=false;
+
+    cin >> team;
+
+    for(i=0; i<team.size(); i++)
+    {
+        if((team[i])=='0')
+        {
+            sum0++;
+            sum1 = 0;
+
+            if(sum0 >= 7)
+            {
+                danger = true;
+                break;
+            }
+        }
+        else
+        {
+            sum1++;
+            sum0 = 0;
+
+            if(sum1 >= 7)
+            {
+                danger = true;
+                break;
+            }
+        }
+    }
+
+    if(danger)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}

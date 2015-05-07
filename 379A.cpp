e1d9611bcb4a0a7a2f939872e2089a0312ee9b41
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-07 14:46:36 +0800

    379A - New Year Candles
    link: http://codeforces.com/problemset/problem/379/A

---
+++ 379A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a, b, hour;
+
+    cin >> a >> b;
+    hour = (a*b-1)/(b-1);
+    cout << hour;
+
+    return 0;
+}

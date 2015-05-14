4de93861a40e00bbe2a0d8db6f573f5e7291f1eb
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-14 13:23:00 +0800

    141A - Amusing Joke
    link: http://codeforces.com/problemset/problem/141/A

---
+++ 141A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string a, b, c;
+    int i;
+    int letter[26]={0};
+
+    cin >> a >> b >> c;
+
+    for(i=0; i<a.size(); i++)
+        letter[a[i]-'A']++;
+
+    for(i=0; i<b.size(); i++)
+        letter[b[i]-'A']++;
+
+    for(i=0; i<c.size(); i++)
+        letter[c[i]-'A']--;
+
+    for(i=0; i<26; i++)
+    {
+        if(letter[i] != 0)
+        {
+            cout << "NO";
+            return 0;
+        }
+    }
+
+    cout << "YES";
+
+    return 0;
+}

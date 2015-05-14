a287451392b816636b1c46d706a93dffc90edd00
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-14 14:49:16 +0800

    318A - Even Odds
    link: http://codeforces.com/problemset/problem/318/A

---
+++ 318A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n, k, i;
+
+    cin >> n >> k;
+
+    if(k<=(n+1)/2)
+        cout << 2*k-1;
+    else
+        cout << 2*(k-(n+1)/2);
+
+    return 0;
+}

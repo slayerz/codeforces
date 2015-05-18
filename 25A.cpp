36ce4ae232dd773bfdf026e30f9dbbd2847ffb04
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-18 13:14:08 +0800

    25A - IQ test
    http://codeforces.com/problemset/problem/25/A

---
+++ 25A.cpp
@@ -0,0 +1,37 @@
+//25A - IQ test
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, s[100];
+    int evenness=0;
+
+    cin >> n;
+
+    for(i=0; i<n; i++)
+    {
+        cin >> s[i];
+
+        if(s[i]%2==0)
+            evenness++;
+    }
+
+    for(i=0; i<n; i++)
+    {
+        if(evenness>1)
+        {
+            if(s[i]%2!=0)
+                cout << i+1;
+        }
+        else
+        {
+            if(s[i]%2==0)
+                cout << i+1;
+        }
+    }
+
+    return 0;
+}

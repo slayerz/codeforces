7156f281b2252d4ce8f4cb19f994578eca963b07
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-19 13:36:38 +0800

    263A - Beautiful Matrix
    link: http://codeforces.com/problemset/problem/263/A

---
+++ 263A.cpp
@@ -0,0 +1,23 @@
+//263A - Beautiful Matrix
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int m[25][25];
+    int i, j;
+
+    for(i=0; i<5; i++)
+        for(j=0; j<5; j++)
+            cin >> m[i][j];
+
+    for(i=0; i<5; i++)
+        for(j=0; j<5; j++)
+            if(m[i][j]==1)
+                cout << abs(i-2) + abs(j-2);
+
+    return 0;
+}

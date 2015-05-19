6230a7de758818fec8dd6c21bcd7619d928055e3
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-19 13:37:24 +0800

    427A - Police Recruits
    link: http://codeforces.com/problemset/problem/427/A

---
+++ 427A.cpp
@@ -0,0 +1,32 @@
+//427A - Police Recruits
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, e;
+    int p=0, c=0;
+
+    cin >> n;
+
+    while(n--)
+    {
+        cin >> e;
+
+        if(e==-1)
+        {
+            if(p==0)
+                c++;
+            else
+                p--;
+        }
+        else
+            p += e;
+    }
+
+    cout << c;
+
+    return 0;
+}

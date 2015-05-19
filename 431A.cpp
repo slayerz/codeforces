28c81663154576de6be99785d1f21990d1b25477
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-19 13:38:18 +0800

    431A - Black Square
    link: http://codeforces.com/problemset/problem/431/A

---
+++ 431A.cpp
@@ -0,0 +1,24 @@
+//431A - Black Square
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    int a[4], i;
+    int sum=0;
+
+    for(i=0; i<4; i++)
+        cin >> a[i];
+
+    cin >> s;
+
+    for(i=0; i<s.size(); i++)
+        sum += a[((char)s[i]-'0')-1];
+
+    cout << sum;
+
+    return 0;
+}

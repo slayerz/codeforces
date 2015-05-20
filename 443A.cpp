db5100fb9ceea163daa4c9ff0848515d7556c975
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-20 11:22:54 +0800

    443A - Anton and Letters
    link: http://codeforces.com/problemset/problem/443/A

---
+++ 443A.cpp
@@ -0,0 +1,26 @@
+//443A - Anton and Letters
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    int i;
+    int d=0, letter[26]={0};
+
+    getline(cin, line);
+
+    for(i=0; i<line.size(); i++)
+        if(isalpha(line[i]))
+            letter[line[i]-'a']++;
+
+    for(i=0; i<26; i++)
+        if(letter[i]>0)
+            d++;
+
+    cout << d;
+
+    return 0;
+}

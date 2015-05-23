252546e081580d349a129f273dbdb6b68d0bce74
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-23 12:12:39 +0800

    510A - Fox And Snake
    link: http://codeforces.com/problemset/problem/510/A

---
+++ 510A.cpp
@@ -0,0 +1,52 @@
+//510A - Fox And Snake
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, i, k, s;
+    int flag=0;
+
+    cin >> n >> m;
+
+    for(i=0; i<n; i++)
+    {
+        s = 0;
+
+        for(k=0; k<m; k++)
+        {
+            if((i+1)%2 == 0)
+            {
+                if(flag==0)
+                {
+                    if(k+1==m && s==0)
+                    {
+                        cout << '#';
+                        flag = 1;
+                    }
+                    else
+                        cout << '.';
+                }
+                else
+                {
+                    if(k==0)
+                    {
+                        cout << '#';
+                        flag = 0;
+                        s = 1;
+                    }
+                    else
+                        cout << '.';
+                }
+            }
+            else
+                cout << '#';
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}

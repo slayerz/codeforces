22b9d7ada956c55140beb0b3b72d3ea7565acfa9
Pikri Mohammad <slayerz.ninja@gmail.com>
2015-05-25 12:11:11 +0800

    467A - George and Accommodation
    link: http://codeforces.com/problemset/problem/467/A

---
+++ 467A.cpp
@@ -0,0 +1,25 @@
+//467A - George and Accommodation
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, p, q;
+    int room=0;
+
+    cin >> n;
+
+    while(n--)
+    {
+        cin >> p >> q;
+
+        if(p+2<=q)
+            room++;
+    }
+
+    cout << room;
+
+    return 0;
+}
